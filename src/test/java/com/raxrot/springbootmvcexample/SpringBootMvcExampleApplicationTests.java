0ec62d4e97141c170e516121cc6a7ae672e606c7
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-10-26 21:54:18 +0100

    init

---
+++ src/test/java/com/raxrot/springbootmvcexample/SpringBootMvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raxrot.springbootmvcexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
